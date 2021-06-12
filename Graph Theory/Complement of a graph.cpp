b02e5b3a9ae44f1831adbc6a7c865c789c3fd5fa
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-06-12 11:34:45 +0600

    Create Complement of a graph.cpp

---
+++ Graph Theory/Complement of a graph.cpp
@@ -0,0 +1,95 @@
+/*
+In graph theory, the complement or inverse of a graph G is a graph H on the same vertices such that two distinct vertices of H are adjacent if and only if they are not adjacent in G.
+sample input 1:
+4 4
+1 2
+2 3
+1 3
+1 4
+4 3
+2 4
+4 3
+3 2
+sample output:
+NO
+sample input 2:
+4 10
+1 2
+2 3
+1 4
+1 3
+3 2
+3 3
+4 4
+2 1
+4 1
+2 3
+4 2
+2 4
+4 3
+sample output:
+YES
+sample input 3:
+4 10
+1 2
+2 3
+1 4
+1 3
+3 2
+3 3
+4 4
+2 1
+4 1
+2 3
+4 3
+2 4
+4 3
+4 4
+sample output:
+NO
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{    
+    ll n, m;
+    cin >> n >> m ;
+
+    set<pair<ll, ll> > g1, g2, s ;
+
+    for(int i=0; i<m; i++)
+    {
+    	int a, b;
+    	cin >> a >> b ;
+    	
+    	if(a>b) 
+    		swap(a, b);
+    	
+    	g1.insert({a, b});
+    	s.insert({a, b});
+    }
+
+    ll p, q;
+    cin >> p >> q ;
+
+    for(int i=0; i<q; i++)
+    {
+    	int a, b;
+    	cin >> a >> b ;
+    	
+    	if(a>b) 
+    		swap(a, b);
+    	
+    	g2.insert({a, b});
+    	s.insert({a, b});
+    }
+
+    if(n != p || g1.size()+g2.size()!=s.size())
+    	cout << "NO" << endl;
+    else
+    	cout << "YES" << endl;
+
+    return 0;
+}

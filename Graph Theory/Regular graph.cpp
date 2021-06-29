322a833476361f02668f52d8df3e5cac9ce181f8
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-06-29 15:35:09 +0600

    Create Regular graph.cpp

---
+++ Graph Theory/Regular graph.cpp
@@ -0,0 +1,19 @@
+/*
+A graph is said to be regular or K-regular if all its vertices have the same degree K. A graph whose all vertices have degree 2 is known as a 2-regular graph.
+
+- A regular graph can be disconnected
+
+Given necessary info about a graph with n vertex and m edges, find if the graph is a k-regular graph. Also print the value of k.
+............................
+Sample input 1:
+6 6
+1 2
+1 3
+1 4
+2 3
+2 4
+3 4
+output:
+Yes
+
+/*

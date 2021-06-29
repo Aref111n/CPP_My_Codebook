697c6982696869d46a3e131b0f8544c659ce46d0
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-06-29 16:33:10 +0600

    Update Regular graph.cpp

--- Graph Theory/Regular graph.cpp
+++ Graph Theory/Regular graph.cpp
@@ -1,12 +1,13 @@
 /*
 A graph is said to be regular or K-regular if all its vertices have the same degree K. A graph whose all vertices have degree 2 is known as a 2-regular graph.
-
 - A regular graph can be disconnected
+- 0-regular graphs have 0 edges
+- A tree can't be a regular graph
 
 Given necessary info about a graph with n vertex and m edges, find if the graph is a k-regular graph. Also print the value of k.
 ............................
 Sample input 1:
-6 6
+4 6
 1 2
 1 3
 1 4
@@ -15,5 +16,58 @@ Sample input 1:
 3 4
 output:
 Yes
-
+3
+............................
+Sample input 2:
+5 0
+output:
+Yes
+0
+............................
+Sample input 3:
+6 5
+1 2
+2 3
+3 1
+4 5
+5 6
+output:
+No
 /*
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+vector<ll> adj[3006];
+
+int main()
+{   
+    ll n, m;
+    cin >> n >> m ;
+
+    for(int i=0; i<m; i++)
+    {
+        int a, b;
+        cin >> a >> b ;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }    
+
+    ll c = 0;
+    for(int i=2; i<=n; i++)
+    {
+        if(adj[i].size()!=adj[1].size())
+        {
+            c = 1;
+            break;
+        }
+    }
+
+    if(c==0)
+        cout << "Yes" << endl << adj[1].size() << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}

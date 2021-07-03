02de0b5f869486bf516ecc8bce82a1f71ad67327
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-07-03 22:18:20 +0600

    Create 019-Hamiltonian path and circuit.cpp

---
+++ Graph Theory/019-Hamiltonian path and circuit.cpp
@@ -0,0 +1,5 @@
+/*
+A Hamiltonian circuit is a circuit that visits every vertex once with no repeats. 
+Being a circuit, it must start and end at the same vertex. 
+A Hamiltonian path also visits every vertex once with no repeats, but does not have to start and end at the same vertex.
+*/

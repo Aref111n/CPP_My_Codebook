7d74a9b87aae94c3a14eb50c8c3be78bcd1c912e
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-08-25 20:36:59 +0600

    Create UVA 459.cpp

---
+++ Disjoint set/Practice/UVA 459.cpp
@@ -0,0 +1,206 @@
+//BISMILLAHIR RAHMANIR RAHIM
+//INNALLAHA_MA_AS_SABIRIN
+ 
+ 
+ 
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+#define      __                 <<" "<<
+#define      loop(m,n)          for(m=0;m<n;m++)
+#define      loop1(m,n)         for(m=1;m<=n;m++)
+#define      rloop(m,n)         for(m=n-1;m>=0;m--)
+#define      say(VAL)           ll VAL; cin >> VAL  
+#define      case()             "Case " << z++ << ": "
+#define      test_inp()         ll Tc,z=1;cin>>Tc;while(Tc--)
+#define      ntest_inp()        for(int Tc=0;Tc<1;Tc++)
+#define      len(n)             (long long)((log(n)/log(10))+1.0000000001)
+#define      PTF(b)             puts(b?"YES":"NO")
+#define      Ptf(b)             puts(b?"Yes":"No")
+#define      ptf(b)             puts(b?"yes":"no")
+#define      rev                greater<ll>
+#define      newline            cout<<endl
+#define      quit               return 0
+ 
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+//typedefs
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+ 
+
+//Function prototypes
+inline void ls();
+inline ll ceill(ll n, ll k=1);
+long long maxx(ll=0,ll=0,ll=0,ll=0,ll=0);
+long long minn(ll=INT_MAX,ll=INT_MAX,ll=INT_MAX,ll=INT_MAX,ll=INT_MAX);
+bool cmp1(pair<ll,ll> i1,pair<ll,ll> i2);
+bool cmp2(pair<ll,ll> a, pair<ll,ll> b);
+long long eugcd(ll a,ll b);
+
+//constants
+const ll mod = 1000000007;
+const ll MX = 100006 ;
+ 
+//Solve here...
+ll parent[1000000];
+ll level[1000000];
+
+void make_set(ll v)
+{
+    parent[v]=v;
+    level[v]=0;
+
+}
+ll find_root(ll v)
+{
+    while(v!=parent[v])
+        v = parent[v];
+    return v;
+}
+void union_set(ll p, ll q)
+{
+    ll x = find_root(p);
+    ll y = find_root(q);
+    if(x==y)
+        return;
+    if(level[x]>level[y])
+        parent[y]=x;
+    else
+    {
+        parent[x]=y;
+        if(level[x]==level[y])
+            level[y]++;
+    }
+}
+
+int main()
+{
+    ls();
+    
+#ifndef ONLINE_JUDGE
+    freopen("inp.txt", "r", stdin);
+    freopen("outp.txt", "w", stdout);
+#endif
+
+    ll i,j,k;
+ 
+    ll t; cin >> t;
+    string s;
+    for(ll tc=0; tc<t; tc++)
+    {
+        char ch, p, q ;
+        if(tc==0)
+            cin >> ch ;
+        else
+            ch = s[0] ;
+
+        ll n = ch - 'A' + 1;
+
+        loop1(i, n) 
+            make_set(i);
+
+        s.clear();
+
+        while(cin>>s)
+        {
+            if(s.size()==1)
+                break;
+
+            ll a = s[0] - 'A' + 1;
+            ll b = s[1] - 'A' + 1;
+
+            union_set(a, b);
+        }
+
+        ll c = 0 ;
+        set<ll>st;
+        loop1(i, n)
+        {
+            st.insert(find_root(i));
+        }
+
+        cout << st.size() << endl;
+        if(tc<t-1)
+        {
+            newline;
+        }
+
+        memset(parent, 0, sizeof parent);
+        memset(level, 0, sizeof level);
+    }
+    quit;
+}
+
+
+ 
+ 
+ 
+ 
+ 
+//.......Functions here......
+ 
+//Faster your code
+inline void ls()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
+//Multiple Maximum
+ll maxx(ll a, ll b, ll c, ll d, ll e)
+{
+    ll ans = a>b ? a : b ;
+    ans = ans > c ? ans : c ;
+    ans = ans > d ? ans : d ;
+    ans = ans > e ? ans : e ;
+ 
+    return ans;
+}
+//Multiple Minimum
+ll minn(ll a, ll b, ll c, ll d, ll e)
+{
+    ll ans = a<b ? a : b ;
+    ans = ans < c ? ans : c ;
+    ans = ans < d ? ans : d ;
+    ans = ans < e ? ans : e ;
+ 
+    return ans;
+}
+//double sort, both key and value in descending order
+//and to make both ascending, use rbegin() and rend()
+//priority: key
+bool cmp1(pair<ll,ll> i1,pair<ll,ll> i2)
+{
+    if(i1.first == i2.first)
+        return (i1.second> i2.second);
+    else
+        return (i1.first >i2.first);
+}
+//double sort, key ascending and value descending
+//for vice versa, use rbegin() and rend()
+//priority: value
+bool cmp2(pair<ll,ll> a, pair<ll,ll> b)
+{
+    if(a.second!=b.second)
+        return (a.second > b.second);
+    else
+        return (a.first < b.first);
+}
+//Euclidean gcd
+ll eugcd(ll a,ll b)
+{
+    if(b==0)
+        return a;
+    ll p=a%b;
+    return eugcd(b,p);
+}
+//ceil
+inline ll ceill(ll n, ll k)
+{
+    return (n/k)+bool(n%k);
+}
+//......Function over

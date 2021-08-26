11f48fddd948392b15ef84234b3e147ed8f7d5cc
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-08-26 09:07:07 +0600

    Create CSES 1649.cpp

---
+++ Segment tree/Practice/CSES 1649.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<ll,ll> pll;
+ 
+ll tab[800006];
+ll arr[200006];
+ 
+void init(ll v, ll b, ll e)
+{
+    if(b==e)
+        tab[v] = arr[b];
+    else
+    {
+        ll l = v*2 , r = v*2 + 1 ;
+        ll m = (b+e)/2 ;
+        init(l, b, m);
+        init(r, m+1, e);
+        tab[v] = min(tab[l], tab[r]) ;
+    } 
+}
+ 
+ll query(ll v, ll b, ll e, ll i, ll j)
+{
+    if(i>e || j<b) return 1e18;
+    if(i<=b && j>=e) return tab[v] ;
+    ll l = v*2 , r = v*2 + 1, m = (b+e)/2 ;
+    return min(query(l, b, m, i, j), query(r, m+1, e, i, j));
+}
+ 
+void update(ll v, ll b, ll e, ll i, ll val)
+{
+    if(i<b || i>e) return; 
+    if(b==i && e==i)
+    {
+        tab[v] = val ;
+        return;
+    }
+    ll l = v*2 , r = v*2 + 1, m = (b+e)/2 ;
+    update(l, b, m, i, val);
+    update(r, m+1, e, i, val);
+    tab[v] = min(tab[l], tab[r]) ;
+}
+ 
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("inp.txt", "r", stdin);
+    freopen("outp.txt", "w", stdout);
+#endif
+    ll n, q;
+    cin >> n >> q;
+ 
+    for(int i=1; i<=n; i++) 
+        cin >> arr[i] ;
+ 
+    init(1, 1, n);
+ 
+    for(int i=0; i<q; i++)
+    {
+        ll b, x, y; 
+        cin >> b >> x >> y ;
+        if(b==2)
+            cout << query(1, 1, n, x, y) << endl;
+        if(b==1)
+            update(1, 1, n, x, y);
+    }
+}

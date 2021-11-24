4af2bf0ca466d76c094d4d27a4b2f0cc04f2d75a
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-11-25 00:00:49 +0600

    Update Limited coins.cpp

--- DP/Coin change/Limited coins.cpp
+++ DP/Coin change/Limited coins.cpp
@@ -1,11 +1,16 @@
-ll coin(ll coins[], ll count[], ll sum) 
+/*
+You have n types of coin coins[1], coins[2],...,coins[n] and each of them are of limited number.
+The amount of each coins are count[1], count[2],....count[n].
+In how many ways you can change m unit of money?
+*/
+ll coin(ll n, ll coins[], ll count[], ll m) 
 {
 
-    ll i, j, n = 3;
-    ll dp[n+1][sum+1] ;
+    ll i, j, n ;
+    ll dp[n+1][m+1] ;
     rep(i, n+1)
     {
-        rep(j, sum+1)
+        rep(j, m+1)
             dp[i][j]= 0 ;
     }
 
@@ -13,20 +18,20 @@ ll coin(ll coins[], ll count[], ll sum)
     dp[0][0] = 1;
     rep1(j, n) 
     {
-        for (int i = 0; i <= sum; i++) {
+        for (int i = 0; i <= m; i++) {
     dp[j][i] += dp[j-1][i];
     }
 
     for (int k = 1; k <= count[j - 1]; k++) 
     {
         int initial = coins[j - 1] * k;
-        for (int i = initial; i <= sum; i++) 
+        for (int i = initial; i <= m; i++) 
         {
             dp[j][i] += dp[j-1][i - initial];
         }
     }
 }
 
-    return dp[n][sum];
+    return dp[n][m];
 
 }
